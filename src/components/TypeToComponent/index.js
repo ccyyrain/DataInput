b55b3b90e5827829777a3fb5bad01d4df7eb9a5e
Dian Yu <lyn-@YD.local>
2022-01-18 20:02:09 -0600

    use ifelse instead of switch

--- src/components/TypeToComponent/index.js
+++ src/components/TypeToComponent/index.js
@@ -19,30 +19,25 @@ const LineContent = styled.div`
   }
 `;
 
-const TypeToComponent = ({ node, label, index }) => {
-  const TypeContent = ({ node, index }) => {
-    switch (node) {
-      case "string":
-        return <input id={index} type="text" />;
-      case "number":
-        return <input id={index} type="number" />;
-      case "boolean":
-        return <input id={index} type="checkbox" style={{ width: "auto" }} />;
-      case Array.isArray(node):
-        return (
-          <select id={index}>
-            {node.map((itm) => (
-              <option value={itm} key={itm}>
-                {itm}
-              </option>
-            ))}
-          </select>
-        );
-      default:
-        return null;
-    }
-  };
+const TypeContent = ({ node, index }) => {
+  if (node === "string") return <input id={index} type="text" />;
+  else if (node === "number") return <input id={index} type="number" />;
+  else if (node === "bollean")
+    return <input id={index} type="checkbox" style={{ width: "auto" }} />;
+  else if (Array.isArray(node) === true)
+    return (
+      <select id={index}>
+        {node.map((itm) => (
+          <option value={itm} key={itm}>
+            {itm}
+          </option>
+        ))}
+      </select>
+    );
+  else return null;
+};
 
+const TypeToComponent = ({ node, label, index }) => {
   return (
     <LineItem index={index}>
       <LineLabel>{label}</LineLabel>
